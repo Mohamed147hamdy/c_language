8b1fb5adb50dc040a2d5a1ef6af4b96d0f55a18d
Mohamed_Hamdy <120296198+Mohamed147hamdy@users.noreply.github.com>
2024-01-13 21:04:00 +0200

    first_commit

--- string_1.c
+++ string_1.c
@@ -3,7 +3,7 @@ void main()
 {
 	char name[]= "mohamed";
 	//must end string ('\0')
-	char n[] = {'q','a','w','\0'}; 
+	char n[] = {'q','a','w','\0'}; //wqwq
 	printf("%s\n",name);
     printf("%s\n",n); 
 	}
